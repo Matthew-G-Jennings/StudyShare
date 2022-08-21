40209c806cedbb8878422a4cf57fa8947897f208
Matthew Jennings <jenma048@student.otago.ac.nz>
2022-08-21 15:51:49 +1200

    Created file of static vars to store information about logged in user. UID is extracted and stored here

--- StudyShare/HomeViewController.swift
+++ StudyShare/HomeViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewController: UIViewController {
 
@@ -14,7 +16,15 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if Auth.auth().currentUser != nil{
+            let user = Auth.auth().currentUser
+            User.UID = user!.uid
+            let db = Firestore.firestore()
+            let userData = db.collection("users").whereField("uid", isEqualTo: User.UID!)
+            
+        } else {
+            // Something has gone horribly wrong
+        }
         // Do any additional setup after loading the view.
     }
     
---
+++ StudyShare/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  StudyShare
+//
+//  Created by Matthew Jennings on 21/08/22.
+//
+
+import Foundation
+
+struct User{
+    static var UID: String? = nil
+    static let firstName: String? = nil
+    static let lastName: String? = nil
+    static let groups: [String]? = nil
+}
