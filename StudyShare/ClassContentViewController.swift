9040602cd695101e0d0699fe91f36718fba6891b
Matthew Jennings <jenma048@student.otago.ac.nz>
2022-08-24 20:11:56 +1200

    Class content screen and related view controller added

---
+++ StudyShare/ClassContentViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ClassContentViewController.swift
+//  StudyShare
+//
+//  Created by Matthew Jennings on 24/08/22.
+//
+
+import UIKit
+
+class ClassContentViewController: UIViewController {
+
+    @IBOutlet weak var classNameLabel: UILabel!
+    @IBOutlet weak var contentTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
