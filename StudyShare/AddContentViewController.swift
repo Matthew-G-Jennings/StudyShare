c38e2f6a77b96205150054cb71ee045abd1036b5
Matthew Jennings <jenma048@student.otago.ac.nz>
2022-08-27 14:39:18 +1200

    Add content now gets a list of files in the apps directory for transcriptions and stores in array

--- StudyShare/AddContentViewController.swift
+++ StudyShare/AddContentViewController.swift
@@ -8,14 +8,33 @@
 import UIKit
 
 class AddContentViewController: UIViewController {
+    
+    var filenames: [String?] = []
 
     @IBOutlet weak var typeSelector: UIButton!
     @IBOutlet weak var contentTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getFileNames()
+        print("DIRECTORY CONTENTS")
+        print(filenames)
     }
     
+    func getFileNames(){
+        let manager = FileManager.default
+        guard let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first else{
+            return
+        }
+        let currDir = url.appendingPathComponent("Transcriptions")
+        print("Current Directory is:")
+        print(currDir)
+        do {
+            self.filenames = try manager.contentsOfDirectory(atPath: currDir.path)
+        } catch {
+            print("An error has occurred in reading directory contents")
+        }
+    }
 
     @IBAction func backTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
