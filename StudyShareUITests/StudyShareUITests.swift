ce0cb9a257bb9dcd8d9247892d3fc8732bf4a0da
zjseales <seaza886@student.otago.ac.nz>
2022-09-24 19:13:58 +1200

    Created UI Tests for Login and SignUp Views.

--- StudyShareUITests/StudyShareUITests.swift
+++ StudyShareUITests/StudyShareUITests.swift
@@ -2,40 +2,102 @@
 //  StudyShareUITests.swift
 //  StudyShareUITests
 //
-//  Created by Matthew Jennings on 31/07/22.
+//  UI Tests for the StudyShareApp
 //
 
 import XCTest
 
 class StudyShareUITests: XCTestCase {
+    
+    private var app: XCUIApplication!
 
+    /** Initializes an instance of the app.
+     */
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // In UI tests it is usually best to stop
+        // immediately when a failure occurs.
         continueAfterFailure = false
+        // launches the app
+        self.app = XCUIApplication()
+        self.app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
+    /** Terminates the app after each test.
+     */
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.app.terminate()
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    /** Ensures buttons on the initial start-up screen exist.
+     */
+    func test_initialView() throws {
+        // Ensure initial start up page contains all UI elements
+        let signUpButton = self.app.buttons["Sign Up"]
+        XCTAssert(signUpButton.exists)
+        
+        let loginButton = self.app.buttons["Login"]
+        XCTAssert(loginButton.exists)
+    }
+    
+    /** Ensures button tap redirects to correct view and that all elements are displayed.
+     */
+    func test_signUpView() throws {
+        // ensure sign up button redirects to the SignUpView
+        let signUpButton = self.app.buttons["Sign Up"]
+        signUpButton.tap()
+        
+        let title = self.app.staticTexts["Create Account"]
+        XCTAssert(title.exists)
+        
+        //ensure all text fields are displayed
+        var newField = self.app.textFields["First Name"]
+        XCTAssert(newField.exists)
+        newField = self.app.textFields["Last Name"]
+        XCTAssert(newField.exists)
+        newField = self.app.textFields["Email"]
+        XCTAssert(newField.exists)
+        newField = self.app.textFields["Password"]
+        //check an invalid text field to further validate the test
+        let falseField = self.app.textFields["Doesn't Exist"]
+        XCTAssert(!falseField.exists)
+        
+        //ensure all buttons are displayed and work as expected.
+        var button = self.app.buttons["Create Account"]
+        XCTAssert(button.exists)
+        button.tap()
+        //check no input error works
+        let errorLabel = self.app.staticTexts["Please fill in all fields"]
+        XCTAssert(errorLabel.exists)
+        
+        // test the back button
+        button = self.app.buttons["Back"]
+        XCTAssert(button.exists)
+        button.tap()
+        //ensure back button redirects to initial screen
+        XCTAssert(signUpButton.exists)
+        let loginButton = self.app.buttons["Login"]
+        XCTAssert(loginButton.exists)
+    }
+    /** Ensure user is redirected to login page when login button is tapped, and that all ui elements exist.
+     */
+    func test_loginView() throws {
+        let loginButton = self.app.buttons["Login"]
+        XCTAssert(loginButton.exists)
+        loginButton.tap()
+        var textF = self.app.textFields["Email"]
+        XCTAssert(textF.exists)
+        textF = self.app.textFields["Password"]
+        XCTAssert(textF.exists)
+        let title = self.app.staticTexts["Sign In"]
+        XCTAssert(title.exists)
+        let backButton = self.app.buttons["Back"]
+        XCTAssert(backButton.exists)
+        backButton.tap()
+        //ensure back button redirects to initial screen
+        XCTAssert(loginButton.exists)
+        let signUpButton = self.app.buttons["Sign Up"]
+        XCTAssert(signUpButton.exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
